981592998465745692837dbf31beb94a823a7a3d
denispalitsyn <den.palitsyn@gmail.com>
2023-09-25 13:12:48 +0400

    create archive tasks added

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-import { getTasks } from './api/task';
+import { getActiveTasks, getArchiveTasks } from './api/task';
 import { getUser } from './api/user';
-import { renderTasks, renderTasksLoader, renderUser } from './utils/renders';
+import {
+  renderActiveTasks,
+  renderActiveTasksLoader,
+  renderArchiveTasks,
+  renderArchiveTasksLoader,
+  renderUser,
+} from './utils/renders';
 import { openTaskModal } from './utils/taskModalHandlers';
-import { onTasksContainerClick } from './utils/tasksContainerHandlers';
+import { onActiveTasksContainerClick, onArchiveTasksContainerClick } from './utils/tasksContainerHandlers';
 
 function removeUserLoader() {
   const loader = document.querySelector('#loader');
@@ -10,6 +16,22 @@ function removeUserLoader() {
   if (loader) {
     loader.remove();
   }
+}
+
+async function getAndRenderActiveTasks() {
+  renderActiveTasksLoader();
+
+  const activeTasks = await getActiveTasks();
+
+  renderActiveTasks(activeTasks);
+}
+
+async function getAndRenderArchiveTasks() {
+  renderArchiveTasksLoader();
+
+  const archiveTasks = await getArchiveTasks();
+
+  renderArchiveTasks(archiveTasks);
 }
 
 async function start() {
@@ -24,18 +46,17 @@ async function start() {
 
     removeUserLoader();
 
-    renderTasksLoader();
+    await getAndRenderActiveTasks();
 
-    const tasks = await getTasks();
-
-    renderTasks(tasks);
+    await getAndRenderArchiveTasks();
 
     const addTaskButton = document.querySelector('#addTaskButton');
-    const tasksContainer = document.querySelector('#tasksContainer');
+    const activeTasksContainer = document.querySelector('#tasksContainer');
+    const archiveTasksContainer = document.querySelector('#archiveTasksContainer');
 
     addTaskButton.addEventListener('click', () => openTaskModal());
-
-    tasksContainer.addEventListener('click', onTasksContainerClick);
+    activeTasksContainer.addEventListener('click', onActiveTasksContainerClick);
+    archiveTasksContainer.addEventListener('click', onArchiveTasksContainerClick);
   } catch (error) {
     console.log('error', error);
   }
