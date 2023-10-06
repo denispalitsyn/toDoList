fcee0eeeba421731b4e689e6556ef8ab8fe8b2bd
denispalitsyn <den.palitsyn@gmail.com>
2023-10-06 13:38:03 +0400

    redirect fixed

--- api/user.js
+++ api/user.js
@@ -5,7 +5,7 @@ export async function register(email, password) {
     email,
     password,
     options: {
-      emailRedirectTo: 'http://localhost:5173/auth/login.html',
+      emailRedirectTo: 'https://denispalitsyn-to-do-list.netlify.app/auth/login.html',
     },
   });
 
